f8d4023019244a7d5dd009eeeb77287895f9046b
WuQiong <cheetah@cheetah.local>
2014-06-06 16:26:34 +0800

    for-in

---
+++ FlowControl.playground/section-1.swift
@@ -0,0 +1,45 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+//..表示一个左闭右开的区间[1 5)
+//...表示一个闭区间[1 5]
+for index in 1...5 {
+    println("\(index) times is \(index * 5)")
+}
+
+let base = 3
+let power = 10
+var answer = 1
+
+for _ in 1...power {
+    answer *= base
+}
+println("\(base) to the power of \(power) is \(answer)")
+
+//遍历数组
+let names = ["Anna", "Jack", "Tom"]
+for name in names {
+    println("Hello, \(name)")
+}
+
+names[1]
+
+//遍历字典
+let numberOfLegs = ["spider":8, "ant":6, "cat":4]
+for (animalName, _) in numberOfLegs {
+    println("\(animalName)s have \(numberOfLegs[animalName])")
+}
+
+numberOfLegs["spider"]
+
+//遍历字符串
+for character in "Hello" {
+    println(character)
+}
+
+var index: Int
+for index = 0; index < 3; index++ {
+    println("index is \(index)")
+}
+println("end index: \(index)")
